09a530756fef5a1989a7143d401422cf27c69aa6
lihoo55555 <ZzzAoo@lhooo01>
2017-05-03 10:29:56 +0800

    'readme'

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 /*引用入口app的vue文件*/
-import App from './App'
+// import App from './App'
 // import router from './router'
 import Router from './config/routes-vaccine' // 路由列表
 import iView from 'iview'            //引入iviewUI
 import 'iview/dist/styles/iview.css' // 使用 iview CSS
-
-  Vue.config.productionTip = false;
+// import './style/theme/index.less'
+//   Vue.config.productionTip = false;
   Vue.use(iView);
   new Vue({
     el: '#app',
